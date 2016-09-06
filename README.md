4e322c8583e163b6473bed8b085c8b03467b8dba
rhuanhuan <584342218@qq.com>
2016-09-06 15:06:24 +0800

    Delete some independent things, add the readme file

---
+++ README.md
@@ -0,0 +1,2 @@
+This is to caculate the final socer of a bowling game.
+The bowling rules can refers to this page: http://code.joejag.com/coding-dojo/bowling-scores/
--- js/bowling.js
+++ js/bowling.js
@@ -1,43 +1,66 @@
-// function roundStatus()
-
-function countEachScores(scoresTable){
-    var frameScores=[];
-    for (i=0;i<9;i++) {
-        if (scoresTable[i][0]==10){
-            if ((scoresTable[i+1][0]==10)&&(i!=8)){
-                frameScores[i]=20+scoresTable[i+2][0];
+var bowlingGame = {
+    scoresTable: [],
+    roundStatus: function(){
+        var roundStatus=[],
+            scoresTable=this.scoresTable;
+        for (i=0;i<=9;i++){
+            if (scoresTable[i][0]==10){
+                roundStatus[i]='strike';
             }
-            else if ((scoresTable[i+1][0]==10)&&(i==8)){
-                frameScores[i]=20+scoresTable[i+1][1];
+            else if (scoresTable[i][0]+scoresTable[i][1]==10){
+                roundStatus[i]='spare';
+            }
+        }
+        return roundStatus;
+    },
+    countFrontRound:function () {
+        var frameScores=[],
+            scoresTable=this.scoresTable,
+            roundStatus=this.roundStatus();
+        for (i=0;i<9;i++) {
+            if (roundStatus[i]=='strike'){
+                if ((roundStatus[i+1]=='strike')&&(i!=8)){
+                    frameScores[i]=scoresTable[i][0]+scoresTable[i+1][0]+scoresTable[i+2][0];
+                }
+                else if ((roundStatus[i+1]=='strike')&&(i==8)){
+                    frameScores[i]=scoresTable[i][0]+scoresTable[i+1][0]+scoresTable[i+1][1];
+                }
+                else{
+                    frameScores[i]=scoresTable[i][0]+scoresTable[i+1][0]+scoresTable[i+1][1];
+                }
+            }
+            else if (roundStatus[i]=='spare'){
+                frameScores[i]=scoresTable[i][0]+scoresTable[i][1]+scoresTable[i+1][0];
             }
             else{
-                frameScores[i]=10+scoresTable[i+1][0]+scoresTable[i+1][1];
+                frameScores[i]= scoresTable[i][0]+scoresTable[i][1];
             }
         }
-        else if (scoresTable[i][0]+scoresTable[i][1]==10){
-            frameScores[i]=10+scoresTable[i+1][0];
+        return frameScores;
+    },
+    countLastRound:function () {
+        var frameScores=0,
+            scoresTable=this.scoresTable;
+        if ((scoresTable[9][0]==10)||(scoresTable[9][0]+scoresTable[9][1]==10)){
+            frameScores = scoresTable[9][0]+scoresTable[9][1]+scoresTable[9][2]
         }
-        else{
-            frameScores[i]= scoresTable[i][0]+scoresTable[i][1];
+        return frameScores;
+    },
+    countScoresList: function () {
+        var frameScores=[];
+        frameScores=this.countFrontRound();
+        frameScores[9]=this.countLastRound();
+        return frameScores;
+    },
+    countFinalSocer: function(){
+        var finalSocer=0,
+            scoreList=this.countScoresList();
+        for (i=0;i<=9;i++){
+            finalSocer=finalSocer+scoreList[i];
         }
+        return finalSocer;
     }
-    if ((scoresTable[9][0]==10)||(scoresTable[9][0]+scoresTable[9][1]==10)){
-        frameScores[9] = scoresTable[9][0]+scoresTable[9][1]+scoresTable[9][2]
-    }
-    return frameScores;
 }
 
-function pluScores(scoresList){
-    for (i=1;i<=9;i++){
-        scoresList[i]+=scoresList[i-1];
-    }
-    return scoresList[9];
-}
-
-function gameScores(scoresTable){
-    var frameScores=countEachScores(scoresTable);
-    return pluScores(frameScores);
-}
-
-a=gameScores([[4,6],[4,6],[4,6],[4,6],[4,6],[4,6],[4,6],[4,6],[4,6],[10,10,4]])
-console.log(a)
+bowlingGame.scoresTable = [[4,6],[4,6],[4,6],[4,6],[4,6],[4,6],[4,6],[4,6],[4,6],[4,6,10]];
+console.log(bowlingGame.countFinalSocer());
