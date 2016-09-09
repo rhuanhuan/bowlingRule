3321c8142cfdf8f7df45e8c91cce2c86b4f79f76
rhuanhuan <1210098674@qq.com>
2016-09-09 15:49:35 +0800

    jasmine on gulp is ready

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,18 @@
 var gulp = require('gulp');
+var jasmine = require('gulp-jasmine');
 
-gulp.task('default', function() {
-    // 将你的默认的任务代码放在这
+// var jshint = require('gulp-jshint');
+// var sass = require('gulp-sass');
+// var concat = require('gulp-concat');
+// var uglify = require('gulp-uglify');
+// var rename = require('gulp-rename');
+
+// gulp.task('default', function() {
+//     // 将你的默认的任务代码放在这
+// });
+
+gulp.task('Bowling', function() {
+    // gulp.src('./spec/support/jasmine.json')
+    gulp.src('./spec/jasmine_examples/*Spec.js')
+        .pipe(jasmine())
 });
